1fc62c6ab9bd3eb69f411ca3788a4265464735e0
Dominic Norris <you@example.com>
2020-09-01 16:44:51 +0100

    Added nav items

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Check bank or building society details',
+  serviceName: 'Verify bank account details',
 
   // Default port that prototype runs on
   port: '3000',
