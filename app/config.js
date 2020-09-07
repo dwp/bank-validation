6ae6cf8f53a04cfcc2e0d06541e33e008b5fa617
Dominic Norris <you@example.com>
2020-09-07 15:43:20 +0100

    Added some routing for search terms

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Verify bank account details',
+  serviceName: 'Validate bank details',
 
   // Default port that prototype runs on
   port: '3000',
