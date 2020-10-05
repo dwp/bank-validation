d2272b1774026de33266b2f17e2af8032e50da61
Dominic Norris <you@example.com>
2020-10-05 10:45:25 +0100

    Added index page, second version of prototype and rearranged a few things

--- app/routes.js
+++ app/routes.js
@@ -3,5 +3,11 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Start folder specific routes
+
+router.use('/sprint-1', require('./views/prototypes/01/_routes'));
+
+// current sprint, remember to add older sprint when adding a new folder!
+router.use('/current', require('./views/prototypes/current/_routes'));
 
 module.exports = router
