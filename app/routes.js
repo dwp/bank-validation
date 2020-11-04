eff729e6e9a5f291cd06259b536b21144302ccd8
Dominic Norris <you@example.com>
2020-11-04 15:06:12 +0000

    Revert "Merge branch 'master' of https://github.com/dommn/bank-validation"
    This reverts commit 72bf0c963f819427896b2788483f28cd253349c5, reversing
    changes made to 8cd088ce522166afcf5588ca018bc06fb73ab7c7.

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.use('/01', require('./views/01/index'));
+router.use('/fraud', require('./views/fraud/index'));
+router.use('/fraud-vertical', require('./views/fraud-vertical/index'));
+router.use('/03', require('./views/03/index'));
 
 module.exports = router
