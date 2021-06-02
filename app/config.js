29d9be75bf8cdfdc1843306eb2e1054e9432c4a9
dommn <47447454+dommn@users.noreply.github.com>
2021-06-02 12:56:45 +0100

    Changed app name and added service down page

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Validate bank details',
+  serviceName: 'Check payment account details',
 
   // Default port that prototype runs on
   port: '3000',
